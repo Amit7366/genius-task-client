017948e44e17772ad3a4f965d51fa453a3defcde
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-12-29 11:26:52 +0600

    task name to add task route on enter press

--- src/Pages/AddTask/AddTask.js
+++ src/Pages/AddTask/AddTask.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const AddTask = () => {
-    return (
-        <div>
-            Add Task
+  const {state} = useLocation();
+  const {name} = state;
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+  };
+
+  return (
+    <div className="grid justify-center">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 my-32">
+        <div className="mb-4">
+          <label className="block text-grey-darker text-sm font-bold mb-2">
+            Task Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            defaultValue={name}
+            placeholder="Task Name"
+          />
         </div>
-    );
+        <div className="mb-4">
+          <label className="block text-grey-darker text-sm font-bold mb-2">
+            Image
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+            type="file"
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Add Task
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddTask;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const [nameError, setNameError] = useState("");
+    const navigate = useNavigate();
+  
+    const handleKeyDown = (event) => {
+      setNameError('');
+      if (event.key === "Enter") {
+        const name = event.target.value;
+  
+        if (name === "") {
+          setNameError("Task name cannot be empty!");
+        } else {
+          navigate("/add-task",{ state: { name: name } });
+        }
+      }
+    };
     return (
-        <div>
-            Home
+        <div className="grid justify-center">
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 my-32">
+          <div className="mb-4">
+            <label className="block text-grey-darker text-sm font-bold mb-2">
+              Task Name
+            </label>
+            <input
+              onKeyDown={handleKeyDown}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Task Name"
+            />
+          </div>
+          <div>{nameError && <p className="text-red-600 text-xs">{nameError}</p>}</div>
+          {/* <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Add Task
+            </button>
+          </div> */}
         </div>
+      </div>
     );
 };
 
